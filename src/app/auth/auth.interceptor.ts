485664f649c33545707dec210d872df29e3c0a41
Tim Hesse <tim.hesse@uni-oldenburg.de>
2021-12-13 13:09:38 +0100

    added interceptor to inject tokens into requests

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AuthStorageService} from "./auth-storage.service";
+
+/** Key for the token. */
+const TOKEN_HEADER_KEY = "Authorization";
+
+/** Interceptor to inject the authorization token into every request. */
+@Injectable(/*
+  // The interceptor should be available everywhere
+  providedIn: 'root'
+}*/)
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authStorage: AuthStorageService) {}
+
+  /**
+   * Injects the token as Bearer authentication into a HttpRequest
+   * @param request The request the token should be injected in
+   * @param next The next handler to operate on the request
+   */
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let authRequest = request;
+    const token = this.authStorage.accessToken;
+    if (token) {
+      authRequest = request.clone({
+        // this updates the header without removing every other header
+        headers: request.headers.set(TOKEN_HEADER_KEY, "Bearer " + token)
+      });
+    }
+    return next.handle(authRequest);
+  }
+}
