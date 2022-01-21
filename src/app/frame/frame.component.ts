e7680ed44096f9a3bd79d233e6f96463f421985b
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-01-21 19:04:36 +0100

    moved frame parts into own component

---
+++ src/app/frame/frame.component.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'app-frame',
+  templateUrl: './frame.component.pug',
+})
+export class FrameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
