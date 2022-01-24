4a5197ad9926c1fa049fc5927b44849fa2116d16
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-01-24 16:08:33 +0100

    added logout to the frame

--- src/app/frame/frame.component.ts
+++ src/app/frame/frame.component.ts
@@ -1,14 +1,26 @@
 import {Component, OnInit} from "@angular/core";
+import {AuthService} from "../auth/auth.service";
+import {AuthStorageService} from "../auth/auth-storage.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-frame',
   templateUrl: './frame.component.pug',
 })
-export class FrameComponent implements OnInit {
+export class FrameComponent {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private authStorage: AuthStorageService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  logout() {
+    this.authService.logout(this.authStorage.accessToken!).subscribe(() => {
+      this.authStorage.clear();
+      this.router.navigate(["login"], {replaceUrl: true})
+        .catch(e => console.error(e));
+    });
   }
 
 }
