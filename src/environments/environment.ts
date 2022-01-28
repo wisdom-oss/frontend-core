8baa5f3b2d1c0ba62ea4f6ccd03607f22e1c8664
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-01-28 17:11:27 +0100

    added redundant free env config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import {environment as commonEnvironment} from "./environment.common";
 
-const baseUrl = "https://wisdom-demo.uol.de/"
-
-export const environment = {
-  production: false,
-  baseUrl,
-  apiUrl: baseUrl + "api/"
-};
+export const environment = Object.assign({}, commonEnvironment, {
+  production: false
+});
 
 /*
  * For easier debugging in development mode, you can import the following file
