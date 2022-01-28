6c62fb8f2acf62a9a3d09bb8afd1dce869900d44
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-01-28 16:32:42 +0100

    doc'ed the AuthGuard and enabled login refreshing

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -14,6 +14,8 @@ import {AuthService} from "./auth.service";
 /**
  * Guard checking if the user is signed in, it needs to have a valid access
  * token to browser further.
+ *
+ * Also tries to refresh the token if the old one is not valid.
  */
 @Injectable({
   providedIn: 'root'
@@ -26,34 +28,80 @@ export class AuthGuard implements CanActivate {
     private router: Router
   ) {}
 
+  /**
+   * Method deciding if a user is allowed to navigate to the requested route.
+   *
+   * This one checks if the current token is still valid.
+   * If the token is not valid it tries to refresh the token.
+   * If no refreshing is the possible the route will be set to the login page.
+   *
+   * @param route
+   * @param state
+   */
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | UrlTree | boolean {
+    // ignore login path per se
     if (route.url[0]?.path === "login") return true;
-    let token = this.authStorage.accessToken;
     let loginUrlTree = this.router.parseUrl("/login");
+
+    // if no token is available the use can't be logged in
+    let token = this.authStorage.accessToken;
     if (!token) return loginUrlTree;
+
+    // wait for the server to reply with a token check
     return new Observable(subscriber => {
       this.authService.validate(token!).subscribe({
         next: response => {
+          // token is valid, you're ready to go
           subscriber.next(true);
           subscriber.complete();
         },
+
         error: errResponse => {
+          // token is invalid
           if (errResponse.status !== 401) {
-            // TODO: handle non 401 codes gracefully
+            // TODO: handle non-401 codes gracefully
             subscriber.next(false);
             subscriber.complete();
             return;
           }
 
-          // TODO: first add refresh try here
+          /**
+           * Small function to avoid code depletes.
+           * Clears the auth storage and sends the user to the login page.
+           */
+          let returnToLogin = () => {
+            this.authStorage.clear();
+            subscriber.next(loginUrlTree);
+            subscriber.complete();
+          };
 
-          this.authStorage.clear();
-          subscriber.next(loginUrlTree);
-          subscriber.complete();
-          return;
+          if (this.authStorage.refreshToken) {
+            // if a refresh token is present, try to refresh
+            this.authService.refresh(this.authStorage.refreshToken).subscribe({
+              next: refreshResponse => {
+                // refresh token was valid, update with new data
+                this.authStorage.accessToken = refreshResponse.access_token;
+                this.authStorage.refreshToken = refreshResponse.refresh_token;
+                this.authStorage.scopes = refreshResponse.scope;
+                this.authStorage.expiresIn = refreshResponse.expires_in;
+                subscriber.next(true);
+                subscriber.complete();
+                return;
+              },
+
+              error: refreshErrResponse => {
+                // token is no valid, send the user to the login page
+                if (refreshErrResponse.status === 401) return returnToLogin();
+                // TODO: handle non-401 status codes gracefully
+              }
+            });
+            return;
+          }
+
+          return returnToLogin();
         }
       });
     });
