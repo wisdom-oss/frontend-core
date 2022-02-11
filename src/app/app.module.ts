3a5d2a4958b14621f7dc929221ed307b41faf242
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-02-11 13:34:04 +0100

    app module now uses the common module correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,26 @@ import {NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {WisdomModule} from "common";
 
 import {AppRoutingModule} from "./app-routing.module";
 import {AppComponent} from "./app.component";
 import {LoginComponent} from "./auth/login/login.component";
 import {AuthInterceptor} from "./auth/auth.interceptor";
-import {IonIconComponent} from "./ion-icon/ion-icon.component";
 import {BaseUrlInterceptor} from "./base-url.interceptor";
 import {FrameComponent} from "./frame/frame.component";
-import {StaticLoader} from "./i18n/StaticLoader";
 import {LangSelectorDirective} from "./i18n/lang-selector.directive";
-import {
-  BulmaIsToggleableDirective
-} from "./util/bulma/bulma-is-toggleable.directive";
+import {StaticLoader} from "./i18n/StaticLoader";
 
 @NgModule({
   declarations: [
     AppComponent,
-    IonIconComponent,
     LoginComponent,
     FrameComponent,
-    BulmaIsToggleableDirective,
     LangSelectorDirective
   ],
   imports: [
+    WisdomModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
