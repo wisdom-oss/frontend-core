89082c27490b640b9e1cdf2c4ed3214970c47420
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-05-27 14:22:12 +0200

    added annotation registration for chartjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Chart} from "chart.js";
+import AnnotationPlugin from "chartjs-plugin-annotation";
 
 /** Component of the app, the main entry point for angular. */
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html"
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   /** The title of the website. */
   title = "WISdoM-OSS";
+
+  ngOnInit(): void {
+    Chart.register(AnnotationPlugin);
+  }
 }
