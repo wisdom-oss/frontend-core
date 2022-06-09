c600d987a77d273f321cbd7eed52fab7ae78b033
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-06-09 13:51:28 +0200

    added leaflet.markercluster assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {Chart} from "chart.js";
 import AnnotationPlugin from "chartjs-plugin-annotation";
+import {Icon} from "leaflet";
 
 /** Component of the app, the main entry point for angular. */
 @Component({
@@ -12,6 +13,7 @@ export class AppComponent implements OnInit {
   title = "WISdoM-OSS";
 
   ngOnInit(): void {
+    Icon.Default.imagePath = "assets/leaflet/images/";
     Chart.register(AnnotationPlugin);
   }
 }
