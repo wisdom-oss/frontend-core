9ad77898dc8608ecbe7211165d5dab66b8f8e751
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-08-19 12:54:11 +0200

    replaced prod baseUrl to be dynamic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 import {environment as commonEnvironment} from "./environment.common";
 
 /** Base url for making requests. */
-const baseUrl = "https://wisdom-demo.uol.de/";
-// TODO: the build process should be able to change this
+const baseUrl = window.location.origin + "/";
 
 /** Environment for the dev mode. */
 export const environment = Object.assign({}, commonEnvironment, {
