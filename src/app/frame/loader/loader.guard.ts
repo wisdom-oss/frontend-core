305fc058e460afbc4819654a073d9c1e1e892cb1
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-08-19 14:32:18 +0200

    Loader Guard now also works on child routes

--- src/app/frame/loader/loader.guard.ts
+++ src/app/frame/loader/loader.guard.ts
@@ -3,22 +3,35 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  UrlTree
+  UrlTree, CanActivateChild
 } from "@angular/router";
 import {Observable} from "rxjs";
 
 import {LoaderService} from "./loader.service";
 
+/**
+ * Guard to be called when a navigation starts.
+ * This will reset the loader.
+ */
 @Injectable({
   providedIn: 'root'
 })
-export class LoaderGuard implements CanActivate {
+export class LoaderGuard implements CanActivate, CanActivateChild {
 
+  /**
+   * Constructor.
+   * @param service Service to report the navigation change
+   */
   constructor(private service: LoaderService) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  /** Report on navigation change for direct routes. */
+  canActivate(): true {
+    this.service.clearLoading();
+    return true;
+  }
+
+  /** Report on navigation change for child routes. */
+  canActivateChild(): true {
     this.service.clearLoading();
     return true;
   }
